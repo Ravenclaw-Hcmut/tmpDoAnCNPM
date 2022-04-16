84983abf44b2ea4e33a09ee5c972c5427c3e6df3
Luan Huynh <luanhuynh2001@gmail.com>
2022-04-16 21:06:53 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!-- <div id="app">
+<p>You clicked {{ numOfClicks }} times.</p>
+<button @click="handleClick()">Click me to increment!</button>
+</div> -->
+
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <Menu msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Menu from './components/menu.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    // HelloWorld,
+    Menu,
+}
 }
 </script>
 
@@ -24,3 +32,4 @@ export default {
   margin-top: 60px;
 }
 </style>
+
---
+++ src/components/menu.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="hello">
+    <input type="text" v-model="name">
+    <h1>{{ name }}</h1>
+		<button @click="isShow = !isShow">toggle</button>
+
+		<div v-show="isShow">
+			<h2 v-bind:id="ids">fffffffffffs</h2>
+		</div>
+    <h1>{{ msg }}</h1>
+
+
+    <input type="text" v-model="addTask">
+	<button @click="tasks.push ({content: addTask, done: false})">Add</button>
+
+	<div v-for = "(task, index) in tasks" :key="index">
+		<input type="checkbox" v-model="task.done">
+		<span :class="{done: task.done}">{{task.content}} </span>
+	</div>
+  </div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			name: 123,
+			isShow: true,
+			ids:'htl',
+			tasks: [
+				{ content: 'Evermore', done:false },
+				{ content: 'Fearless', done:false },
+				{ content: 'Red', done:false },
+				{ content: 'Reputation', done:false },
+			],
+			addTask:'',
+		}
+	},
+	methods: {
+		fAdd: function() {
+			this.tasks.push({
+				content: this.addTask, done: false
+			})
+		}
+	},
+  props: {
+    msg: String,
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.done {
+	text-decoration: line-through;
+}
+</style>
